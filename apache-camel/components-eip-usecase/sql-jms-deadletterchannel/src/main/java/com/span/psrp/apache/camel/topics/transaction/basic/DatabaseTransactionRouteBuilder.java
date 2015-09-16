ae7e27837ea64c69d9072e2eb9438b7336685cd1
jaganmohan <jagandudehyd@gmail.com>
2015-09-16 18:28:50 +0530

    PSRP:java dsl example is added

--- apache-camel/components-eip-usecase/sql-jms-deadletterchannel/src/main/java/com/span/psrp/apache/camel/topics/transaction/basic/DatabaseTransactionRouteBuilder.java
+++ apache-camel/components-eip-usecase/sql-jms-deadletterchannel/src/main/java/com/span/psrp/apache/camel/topics/transaction/basic/DatabaseTransactionRouteBuilder.java
@@ -3,13 +3,13 @@ package com.span.psrp.apache.camel.topics.transaction.basic;
 import org.apache.camel.builder.RouteBuilder;
 
 public class DatabaseTransactionRouteBuilder extends RouteBuilder {
+	
     @Override
     public void configure() throws Exception {
-        from("direct:transacted")
-            .transacted()
+        from("direct:transacted").transacted()
             .log("Processing message: ${body}")
-            .setHeader("message", body())
-            .to("sql:insert into audit_log (message) values (:#message)")
+            .setHeader("message", body()).beanRef("auditLogHelper", "generateNewTopic")
+            .to("sql:insert into audit_log (audit_id,message) values (:#auditid,:#auditUser)")
             .to("mock:out");
     }
 }
