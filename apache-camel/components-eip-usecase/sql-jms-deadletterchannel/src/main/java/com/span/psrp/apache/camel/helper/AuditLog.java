14e36b29f1aa14f8696495e413652dea441268a7
jaganmohan <jagandudehyd@gmail.com>
2015-09-22 18:49:58 +0530

    PSRP:sq-jms-deadletterchannel project is updated

---
+++ apache-camel/components-eip-usecase/sql-jms-deadletterchannel/src/main/java/com/span/psrp/apache/camel/helper/AuditLog.java
@@ -0,0 +1,21 @@
+package com.span.psrp.apache.camel.helper;
+
+public class AuditLog {
+
+	private Integer auditd;
+	private String message;
+	public Integer getAuditd() {
+		return auditd;
+	}
+	public void setAuditd(Integer auditd) {
+		this.auditd = auditd;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
---
+++ apache-camel/components-eip-usecase/sql-jms-deadletterchannel/src/main/java/com/span/psrp/apache/camel/helper/AuditLogHelper.java
@@ -0,0 +1,34 @@
+package com.span.psrp.apache.camel.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.camel.Exchange;
+
+public class AuditLogHelper {
+
+	private Random ran = new Random();
+
+    /**
+     * Generates a new topic structured as a {@link Map}
+     */
+    public Map<String, Object> generateNewTopic(Exchange ex){
+    	System.out.print("enterd new Topic"+"generateNewTopic"+"\n");
+    	AuditLog audit=ex.getIn().getBody(AuditLog.class);
+        Map<String, Object> answer = new HashMap<String, Object>();
+         answer.put("auditid", new Integer(ran.nextInt()));
+        answer.put("auditUser", audit.getMessage());
+        return answer;
+    }
+    
+    public Map<String, Object> generateNewTopic1(Exchange ex){
+    	System.out.print("enterd new Topic"+"generateNewTopic"+"\n");
+    	Map<String, Object> answer1 =ex.getIn().getBody(Map.class);
+    	System.out.print("enterd new Topic"+"generateNewTopic"+"\n");
+        Map<String, Object> answer = new HashMap<String, Object>();
+         answer.put("auditid", new Integer(ran.nextInt()));
+        answer.put("auditUser", answer1.get("auditUser"));
+        return answer;
+    }
+}
