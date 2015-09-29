354dced2bd4ca1a048d6dc8d8f5c79a417ccee61
Dinesh Kilari <Dinesh.Kilari@evry.com>
2015-09-29 18:04:38 +0530

    PSRP: apache camel project is updated

---
+++ apache-camel/camel-core/src/main/java/com/span/psrp/apache/camel/topics/properties with custom expression/customexpression.java
@@ -0,0 +1,22 @@
+package com.sample.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+ 
+public class CustomExpression implements Expression {
+
+    @Override
+    public <T> T evaluate(Exchange exchange, Class<T> type) {
+
+        String body = exchange.getIn().getBody(String.class);
+        Object mydata;
+        if (body.contains("amount")) {
+            mydata = body.substring(7, body.length());
+        } else {
+            mydata = 5;
+
+        }
+
+        return exchange.getContext().getTypeConverter().convertTo(type,mydata);
+    }
+}
---
+++ apache-camel/camel-core/src/main/java/com/span/psrp/apache/camel/topics/properties with custom expression/samplecamel.java
@@ -0,0 +1,40 @@
+package com.sample.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+ 
+public class SampleCamel {
+
+    private static String PACKAGE_NAME = SampleCamel.class.getPackage().getName();
+    private static CustomExpression CUST_EXP = new CustomExpression();
+    private static String ORIGINAI_REQUEST_PROPERTY = PACKAGE_NAME +"ORIGINAI_REQUEST_PROPERTY";
+    public static void main(String[] args) throws  Exception{
+
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:src/data/inbox?noop=true").process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        exchange.setProperty(ORIGINAI_REQUEST_PROPERTY, CUST_EXP.evaluate(exchange, Integer.class));
+                    }
+                })
+
+                        .to("file:src/data/outbox").process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        Integer myint = (Integer)exchange.getProperty(ORIGINAI_REQUEST_PROPERTY);
+                        System.out.println(myint);
+                    }
+                });
+            }
+        });
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
+    }
+}
