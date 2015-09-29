403f91375a8caaa0691a82b72cdfb733e58fb56f
Dinesh Kilari <Dinesh.Kilari@evry.com>
2015-09-29 21:18:33 +0530

    PSRP: apache camel project is updated

---
+++ apache-camel/camel-eip/src/main/java/com/span/psrp/apache/camel/topics/transformation/differentwaysofrouting/messagefilter/predicate/PredicateFilter.java
@@ -0,0 +1,42 @@
+package com.sample.camel;
+
+import org.apache.camel.*;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultProducerTemplate;
+
+ 
+public class PredicateFilter {
+    public static void main(String[] args) throws Exception {
+        CamelContext camelContext = new DefaultCamelContext();
+        try {
+            camelContext.addRoutes(new RouteBuilder() {
+                public void configure() {
+                    from("direct:start")
+                            .filter(new Predicate() {
+
+                                public boolean matches(Exchange exchange) {
+                                    final String body = exchange.getIn().getBody(String.class);
+                                    return ((body != null) && body.startsWith("Camel"));
+                                }})
+                            .to("file:src/data/outbox").process(new Processor() {
+                        @Override
+                        public void process(Exchange exchange) throws Exception {
+                            System.out.println(exchange);
+                        }
+                    })
+                    ;
+                }});
+            camelContext.start();
+            ProducerTemplate template = new DefaultProducerTemplate(camelContext);
+            template.start();
+            template.sendBody("direct:start", "Camel Multicast");
+            template.sendBody("direct:start", "Camel Components");
+            template.sendBody("direct:start", "Spring Integration");
+            Thread.sleep(5000);
+        } finally {
+            camelContext.stop();
+        }
+    }
+
+}
