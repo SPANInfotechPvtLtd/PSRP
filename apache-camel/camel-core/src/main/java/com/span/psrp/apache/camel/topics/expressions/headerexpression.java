79dd7523f6f45615ab424a58ba210e0b796c7afc
Dinesh Kilari <Dinesh.Kilari@evry.com>
2015-09-29 18:02:16 +0530

    PSRP:apachecamelprojectisupdated

---
+++ apache-camel/camel-core/src/main/java/com/span/psrp/apache/camel/topics/expressions/headerexpression.java
@@ -0,0 +1,26 @@
+package com.sample.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+ 
+public class HeaderExpression {
+    public static void main(String[] a) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+
+                from("file:src/data/inbox/dini?noop=true")
+                        .choice()
+                        .when(header("CamelFileName").endsWith(".txt"))
+                        .to("file:src/data/outbox")
+                        .otherwise()
+                        .to("file:src/data/others");
+            }
+        });
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
+    }
+}
---
+++ apache-camel/camel-core/src/main/java/com/span/psrp/apache/camel/topics/expressions/simpleexpression.java
@@ -0,0 +1,31 @@
+package com.sample.camel;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+ 
+
+ 
+public class SimpleExpression {
+     public static void main(String[] a) throws Exception {
+        CamelContext camelContext = new DefaultCamelContext();
+           camelContext.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+
+                from("file:src/data/inbox/dini?noop=true")
+                        .choice()
+                        .when(simple("${body} contains 'Dinesh'"))
+                        .to("file:src/data/outbox")
+                        .otherwise()
+                        .to("file:src/data/others");
+            }
+        });
+         camelContext.start();
+        Thread.sleep(10000);
+         camelContext.stop();
+    }
+}
